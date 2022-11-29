b7506acc74be0ef77f81d09db57eea80cc360ec6
Sakibur Rahaman <sakib.cse11.cuet@gmail.com>
2022-11-29 16:59:05 +0600

    Adde comment in user model

--- models/User.php
+++ models/User.php
@@ -1,6 +1,7 @@
 <?php
-require_once __DIR__.'/../framework/storage/Model.php';
+require_once __DIR__ . '/../framework/storage/Model.php';
 
+// User model
 class User extends Model
 {
     public static $tableName = 'user';
