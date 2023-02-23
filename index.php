c4c93a6952876550f70b633d6251201994f7f67b
pavel-sahdat <chymdpavel@gmail.com>
2023-02-24 00:38:18 +0600

    index.php and viewEngine are modifi

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require_once './framework/router/RouteDispatcher.php';
 try {
     $dispatcher = new RouteDispatcher();
     $dispatcher->dispatch();
-} catch (Exception $exception) {
+} catch (Exception $exception){
     echo $exception->getMessage();
 }
