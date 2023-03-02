88fcd0c29560853c4b7b6bc2e0515d98e503dbfe
pavel-sahdat <chymdpavel@gmail.com>
2023-03-02 12:08:19 +0600

    Dashboard controller are successfully done

--- controllers/DashBoardController.php
+++ controllers/DashBoardController.php
@@ -1,18 +1,29 @@
 <?php
 
 require_once __DIR__ . '/Controller.php';
+require_once __DIR__ . '/../models/User.php';
+require_once __DIR__ . '/../models/School.php';
 require_once __DIR__ . '/../models/Office.php';
+
 
 class DashBoardController extends Controller{
     function getCountAll()
     {
         // fetchAll() function use array count
-        // $user = user::fetchAll();
-        // $school = School::fetchAll();
+        $user = user::fetchAll();
         $school = School::fetchAll();
+        $office = Office::fetchAll();
+
+        $userCount = count($user);
+        $schoolCount = count($school);
+        $officeCount = count($office);
+        // $totalCount = array($userCount,$schoolCount,$officeCount);
         
-        $count = count($school);
-        echo "User Count is ".$count."\n";
+        // foreach($totalCount as $count){
+        //     echo "Count = ".$count."\n\n";
+        // }
+        echo " Total User : ".$userCount ."<br>"."Total School: ". $schoolCount
+        ."<br>"."Total Office :". $officeCount;
     }
 }
 
